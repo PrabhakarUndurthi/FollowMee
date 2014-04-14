f9b1c56b7790bb5767c350881f54d76dbde000fa
prabhakar <undurthi.prabhakar@gmail.com>
2014-04-15 01:10:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FollowMe::Application.config.secret_key_base = '582acd2a3281ecacf97771393b04bfd0d1cf5a49fb6c671c4ede22bfdd8098274ff86715cdd072956bbf66d202278cd3f1667ffe31a5c29937c32d8e70c5ff78'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FollowMe::Application.config.session_store :cookie_store, key: '_FollowMe_session'
